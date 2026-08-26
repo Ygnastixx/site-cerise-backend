0e8cb15090ceeb037a10ff6ed6bd54f2f2723627
sydonienadia <sydonienadia@gmail.com>
2026-08-26 21:06:27 +0200

    course v_01

--- courses/urls.py
+++ courses/urls.py
@@ -3,3 +3,12 @@ from django.urls import path
 urlpatterns = [
     # Tes routes viendront ici
 ]
+
+from rest_framework.routers import DefaultRouter
+from .views import CourseViewSet, SectionViewSet
+
+router = DefaultRouter()
+router.register(r"sections", SectionViewSet, basename="section")
+router.register(r"", CourseViewSet, basename="course")
+
+urlpatterns = router.urls
